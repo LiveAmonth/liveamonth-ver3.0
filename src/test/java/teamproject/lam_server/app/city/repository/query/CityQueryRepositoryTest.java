22e55bd03d2d964fc6fbe1029ad850ed11d825f5
Leeky0615 <rbdus7174@naver.com>
2022-01-19 16:25:26 +0900

    test : test city query repository

--- src/test/java/teamproject/lam_server/app/city/repository/query/CityQueryRepositoryTest.java
+++ src/test/java/teamproject/lam_server/app/city/repository/query/CityQueryRepositoryTest.java
@@ -1,54 +1,74 @@
 package teamproject.lam_server.app.city.repository.query;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
-import teamproject.lam_server.app.city.dto.CityGridDataResponse;
-import teamproject.lam_server.app.city.entity.City;
 import teamproject.lam_server.app.city.entity.CityInfo;
-import teamproject.lam_server.app.city.repository.core.CityWeatherRepository;
-import teamproject.lam_server.constants.CategoryConstants;
+import teamproject.lam_server.constants.CategoryConstants.CityInfoCategory;
+import teamproject.lam_server.constants.CategoryConstants.CityName;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static teamproject.lam_server.constants.CategoryConstants.CityInfoCategory.INTRO;
 
 @SpringBootTest
 @Sql(scripts = {"classpath:data.sql"})
-@Transactional
 @Slf4j
 class CityQueryRepositoryTest {
-    @Autowired
-    EntityManager em;
+
     @Autowired
     CityQueryRepository cityQueryRepository;
 
-    @Autowired
-    CityWeatherRepository cityWeatherRepository;
-
-    public CityQueryRepositoryTest() {
-    }
-
-    private static <T extends City> void log(T t) {
-        log.info("city info={}", t.toString());
-    }
-
 
     @Test
-    public void findCityInfo(){
-        List<CityGridDataResponse> cityInfo = cityQueryRepository.findCityInfo(INTRO, CategoryConstants.Month.AUGUST);
-        cityInfo.forEach(c -> log.info("result={}",c));
-        assertThat(cityInfo.size()).isEqualTo(6);
+    @DisplayName("도시이름과 카테고리로 도시 정보 가져오기")
+    public void findCityInfo_all_param() {
+        //given
+        CityName cityName = CityName.SEOUL;
+        CityInfoCategory category = INTRO;
+
+        //when
+        List<CityInfo> cityInfos = cityQueryRepository.findCityInfo(cityName, category);
+
+        //then
+        cityInfos.forEach(cityInfo -> {
+            assertThat(cityInfo.getCityInfoCat()).isEqualTo(category);
+            assertThat(cityInfo.getName()).isEqualTo(cityName);
+        });
     }
 
     @Test
-    public void findCitySlide(){
-        List<CityInfo> citySlideImage = cityQueryRepository.findCityIntroImage(INTRO);
-        citySlideImage.forEach(CityQueryRepositoryTest::log);
+    @DisplayName("도시이름으로 도시 정보 가져오기")
+    public void findCityInfo_cityName() {
+        //given
+        CityName cityName = CityName.SEOUL;
+
+        //when
+        List<CityInfo> cityInfos_cityName = cityQueryRepository.findCityInfo(cityName, null);
+
+        //then
+        cityInfos_cityName.forEach(cityInfo -> {
+            assertThat(cityInfo.getName()).isEqualTo(cityName);
+        });
     }
+    @Test
+    @DisplayName("카테고리로 도시 정보 가져오기")
+    public void findCityInfo_category() {
+        //given
+        CityInfoCategory category = INTRO;
+
+        //when
+        List<CityInfo> cityInfos = cityQueryRepository.findCityInfo(null, category);
+
+        //then
+        cityInfos.forEach(cityInfo -> {
+            assertThat(cityInfo.getCityInfoCat()).isEqualTo(category);
+        });
+    }
+
+
 }
