f1053a058377f636ea6a690a0b5e3e52de6d2d93
Leeky0615 <rbdus7174@naver.com>
2022-01-26 16:05:19 +0900

    chore : setting locale

--- src/main/java/teamproject/lam_server/LamServerApplication.java
+++ src/main/java/teamproject/lam_server/LamServerApplication.java
@@ -1,16 +1,19 @@
 package teamproject.lam_server;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import teamproject.lam_server.config.AopConfig;
 import teamproject.lam_server.trace.logtrace.LogTrace;
 import teamproject.lam_server.trace.logtrace.ThreadLocalLogTrace;
 
 import javax.persistence.EntityManager;
+import java.util.Locale;
 
 @SpringBootApplication
 @Import(AopConfig.class)
@@ -29,5 +32,12 @@ public class LamServerApplication {
     @Bean
     public JPAQueryFactory queryFactory(EntityManager em){
         return new JPAQueryFactory(em);
+    }
+
+    @Bean
+    public LocaleResolver localResolver(){
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.KOREA);
+        return resolver;
     }
 }
