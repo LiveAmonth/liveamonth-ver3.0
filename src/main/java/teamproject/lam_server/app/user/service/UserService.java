e4b7bdb2a2088ac519920ff5ed35f256f2f2c6ca
Leeky0615 <rbdus7174@naver.com>
2022-02-19 21:00:18 +0900

    feat : delete class(refactoring entity name)

--- src/main/java/teamproject/lam_server/app/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package teamproject.lam_server.app.user.service;
-
-import teamproject.lam_server.app.city.dto.CreateCityInfoRequest;
-import teamproject.lam_server.app.user.dto.SimpleUserResponse;
-
-public interface UserService {
-
-    SimpleUserResponse save(CreateCityInfoRequest request);
-}
