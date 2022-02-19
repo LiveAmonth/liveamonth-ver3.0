d9667dd0482806b6418b97fe5350f42eb3c3d4c1
Leeky0615 <rbdus7174@naver.com>
2022-02-19 21:11:09 +0900

    feat : add token exception

--- src/main/java/teamproject/lam_server/global/exception/CustomExceptionHandler.java
+++ src/main/java/teamproject/lam_server/global/exception/CustomExceptionHandler.java
@@ -11,9 +11,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import teamproject.lam_server.app.user.exception.AlreadyDropUserException;
-import teamproject.lam_server.app.user.exception.NormalUserDeleteException;
-import teamproject.lam_server.app.user.exception.UserNotFoundException;
+import teamproject.lam_server.app.member.exception.AlreadyDropUserException;
+import teamproject.lam_server.app.member.exception.NormalUserDeleteException;
+import teamproject.lam_server.app.member.exception.TokenException;
+import teamproject.lam_server.app.member.exception.UserNotFoundException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -31,14 +32,14 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity(createExceptionResponse(ex, request), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
-        return new ResponseEntity(createExceptionResponse(ex, request), HttpStatus.NOT_FOUND);
-    }
-
     @ExceptionHandler({AlreadyDropUserException.class, NormalUserDeleteException.class})
     public final ResponseEntity<Object> handleAlreadyDropUserException(Exception ex, WebRequest request) {
         return new ResponseEntity(createExceptionResponse(ex, request), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler({TokenException.class,UserNotFoundException.class})
+    public final ResponseEntity<Object> handleTokenException(Exception ex, WebRequest request) {
+        return new ResponseEntity(createExceptionResponse(ex, request), HttpStatus.BAD_REQUEST);
     }
 
     private ExceptionResponse createExceptionResponse(Exception ex, WebRequest request) {
