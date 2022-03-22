3dcf0cd889535c0e916533cc723244a82cbd0dc7
Leeky0615 <rbdus7174@naver.com>
2022-03-22 18:38:21 +0900

    feat: remove city name attribute

--- src/main/java/teamproject/lam_server/app/city/dto/CityTransportResponse.java
+++ src/main/java/teamproject/lam_server/app/city/dto/CityTransportResponse.java
@@ -9,14 +9,12 @@ import static teamproject.lam_server.constants.CategoryConstants.*;
 @Data
 public class CityTransportResponse {
     private Long id;
-    private String name;
     private String category;
     private int station_count;
     private int score;
 
     public CityTransportResponse(CityTransport cityTransport) {
         this.id = cityTransport.getId();
-        this.name = cityTransport.getName().getCode();
         this.category = cityTransport.getCityTransportCat().getCode();
         this.station_count = cityTransport.getStationCount();
         this.score = cityTransport.getScore();
--- src/main/java/teamproject/lam_server/app/city/dto/CityWeatherResponse.java
+++ src/main/java/teamproject/lam_server/app/city/dto/CityWeatherResponse.java
@@ -7,16 +7,12 @@ import teamproject.lam_server.constants.CategoryConstants.CityName;
 
 @Data
 public class CityWeatherResponse {
-    private Long id;
-    private CityName name;
     private String month;
     private float maxDegree;
     private float minDegree;
     private float averageDegree;
 
     public CityWeatherResponse(CityWeather cityWeather) {
-        this.id = cityWeather.getId();
-        this.name = cityWeather.getName();
         this.month = cityWeather.getMonth().getCode();
         this.maxDegree = cityWeather.getMaxDegree();
         this.minDegree = cityWeather.getMinDegree();
