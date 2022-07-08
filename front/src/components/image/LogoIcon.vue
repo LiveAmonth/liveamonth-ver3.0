089013c3e1de9d2a0b314a2032da4197a819f431
Leeky0615 <rbdus7174@naver.com>
2022-07-08 17:59:26 +0900

    fix: change logo size

--- front/src/components/image/LogoIcon.vue
+++ front/src/components/image/LogoIcon.vue
@@ -1,7 +1,19 @@
 <template>
-  <el-image style="width: 100px; height: 100px" :src="Logo" fit="contain"/>
+  <div class="logo-container">
+    <el-image
+      style="width: 128px; height: 55px"
+      :src="Logo"
+      fit="fill"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import Logo from "@/assets/image/logo.png";
 </script>
+
+<style>
+/*.logo-container {*/
+/*  height: 55px;*/
+/*}*/
+</style>
