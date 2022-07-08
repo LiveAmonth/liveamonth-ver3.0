bda9f31e846dd65c5c2c8367bbe4bff179c179a8
Leeky0615 <rbdus7174@naver.com>
2022-07-08 17:59:04 +0900

    feat: add i18n

--- front/src/main.ts
+++ front/src/main.ts
@@ -8,13 +8,25 @@ import "normalize.css";
 
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import { ElConfigProvider } from "element-plus";
+import koKR from "element-plus/lib/locale/lang/ko";
 
 import "bootstrap/dist/css/bootstrap-utilities.min.css";
 
+import { createI18n } from "vue-i18n";
+import messages from "./i18n";
+
 const app = createApp(App);
+const i18n = createI18n({
+  locale: "ko",
+  fallbackLocale: "en",
+  messages,
+});
 
 app.use(createPinia());
-app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: koKR
+});
 app.use(router);
-
+app.use(i18n);
 app.mount("#app");
