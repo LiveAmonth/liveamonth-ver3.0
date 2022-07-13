45c1de978ad76daaa6de5128d05cad0f61af2129
Leeky0615 <rbdus7174@naver.com>
2022-07-13 15:55:21 +0900

    fix: apply prettier

--- front/src/i18n/index.ts
+++ front/src/i18n/index.ts
@@ -1,10 +1,10 @@
 import ko from "@/locales/ko_KR.json";
 import en from "@/locales/en_US.json";
-import {createI18n} from "vue-i18n";
+import { createI18n } from "vue-i18n";
 
 const i18n = createI18n({
-    locale: "ko",
-    fallbackLocale: "en",
-    messages: {ko, en}
+  locale: "ko",
+  fallbackLocale: "en",
+  messages: { ko, en },
 });
 export default i18n;
