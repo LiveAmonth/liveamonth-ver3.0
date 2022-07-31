aa90f0a9085faa36ce66047b70d4c5bcf83619d8
Leeky0615 <rbdus7174@naver.com>
2022-07-31 15:03:25 +0900

    feat: add findId, findPw

---
+++ front/src/components/form/FindPwResult.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useMemberStore } from "@/stores/member";
+const store = useMemberStore();
+</script>
+<template>
+  <div class="result-content mb-4">
+    <p class="result-info">{{ $t("form.message.findId.title") }}</p>
+    <div class="result-box d-flex justify-content-between p-2">
+      <p class="result-id">{{ store.foundId.loginId }}</p>
+      <p class="created-date">
+        {{ $t("form.message.findId.created") }} : {{ store.foundId.created }}
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.result-content {
+  .result-info {
+    font-size: 0.8rem;
+  }
+
+  .result-box {
+    border: 0.12rem solid #bbbbbb;
+    align-content: center;
+    .result-id {
+      font-weight: bold;
+    }
+    .created-date {
+      padding-top: 0.5rem;
+      font-size: 0.75rem;
+      color: #383838;
+    }
+  }
+}
+</style>
--- src/main/java/teamproject/lam_server/domain/member/dto/response/FindIdResponse.java
+++ src/main/java/teamproject/lam_server/domain/member/dto/response/FindIdResponse.java
@@ -1,18 +1,26 @@
 package teamproject.lam_server.domain.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import teamproject.lam_server.domain.member.entity.Member;
 import teamproject.lam_server.util.StringUtil;
+
+import java.time.LocalDate;
 
 @Getter
 @Builder
 public class FindIdResponse {
     private String loginId;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+    private LocalDate created;
+
     public static FindIdResponse of(Member member) {
         return FindIdResponse.builder()
-                .loginId(StringUtil.coverContent(member.getLoginId())).build();
+                .loginId(StringUtil.coverContent(member.getLoginId()))
+                .created(member.getCreatedDate().toLocalDate())
+                .build();
     }
 
 }
