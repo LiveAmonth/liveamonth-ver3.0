b0a033584ce53c0abe5b05160a5782ac5f89eae4
Leeky0615 <rbdus7174@naver.com>
2022-08-01 14:53:06 +0900

    fix: add bad credential(wrong password) exception

--- src/main/java/teamproject/lam_server/auth/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/teamproject/lam_server/auth/exception/CustomAuthenticationEntryPoint.java
@@ -1,7 +1,10 @@
 package teamproject.lam_server.auth.exception;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import teamproject.lam_server.exception.ErrorCode;
+import teamproject.lam_server.global.dto.CustomResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +14,18 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        if (authException instanceof BadCredentialsException) {
+            setResponse(response,ErrorCode.BAD_CREDENTIALS);
+        }else{
+            setResponse(response, ErrorCode.ARGUMENTS_NOT_VALID);
+        }
+
+    }
+
+    private void setResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(errorCode.getStatusCode());
+
+        response.getWriter().print(CustomResponse.fail(errorCode));
     }
 }
