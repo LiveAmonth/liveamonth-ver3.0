920ab1208260cf5986c01f8df65518bcbd429079
Leeky0615 <rbdus7174@naver.com>
2022-08-31 17:38:25 +0900

    feat: add base entity column to the insert query

--- src/main/java/teamproject/lam_server/domain/interaction/repository/review/ReviewCommentReactRepository.java
+++ src/main/java/teamproject/lam_server/domain/interaction/repository/review/ReviewCommentReactRepository.java
@@ -16,7 +16,7 @@ public interface ReviewCommentReactRepository extends JpaRepository<ReviewCommen
     @Modifying
     @Transactional
     @Query(value = "" +
-            "insert into review_comment_react (created_date, last_modified_date, to_review_comment_id, type) " +
+            "insert into review_comment_react (created_date, last_modified_date, from_member_id, to_review_comment_id, type) " +
             "values(now(), now(), :#{#request.from}, :#{#request.to}, :type)"
             , nativeQuery = true)
     void react(@Param("request") InteractionRequest request, @Param("type") ReactType type);
