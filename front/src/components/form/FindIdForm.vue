045f4b8c8be6ddee48cdbbb52755328267923362
Leeky0615 <rbdus7174@naver.com>
2022-09-20 13:42:11 +0900

    fix: remove validate

--- front/src/components/form/FindIdForm.vue
+++ front/src/components/form/FindIdForm.vue
@@ -1,49 +1,32 @@
 <script lang="ts" setup>
-import { reactive, ref } from "vue";
-import { useFormValidate } from "@/composables/formValidate";
-import type { FormInstance, FormRules } from "element-plus/es";
-import type { FindIdType } from "@/modules/types/form/FormType";
-import { useRouter } from "vue-router";
+import { reactive } from "vue";
 import { useMessageBox } from "@/composables/messageBox";
 import { useMember } from "@/composables/member";
 import { useI18n } from "vue-i18n";
-import { useMemberStore } from "@/stores/member";
+import type { FindIdType } from "@/modules/types/form/FormType";
 
-const store = useMemberStore();
-const { t } = useI18n();
+const emit = defineEmits(["findId"]);
+
+const { error, isPending, foundId, findId } = useMember();
 const { openMessageBox } = useMessageBox();
-const { error, isPending, findId } = useMember();
-const { validateRequire } = useFormValidate();
-
-const emit = defineEmits(["find-id"]);
-const ruleFormRef = ref<FormInstance>();
-const rules = reactive<FormRules>({
-  name: [validateRequire("member.name")],
-  email: [validateRequire("member.email")],
-});
+const { t } = useI18n();
 
 const findIdForm = reactive<FindIdType>({
   name: "",
   email: "",
 });
 
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
-  await formEl.validate(async (valid) => {
-    if (valid) {
-      await findId(findIdForm);
-      if (error.value) {
-        await openMessageBox(t("form.message.noMember"));
-        for (const key in findIdForm) {
-          findIdForm[key] = "";
-        }
-      } else if (store.foundId) {
-        emit("find-id");
-      }
-    } else {
-      await openMessageBox(t("form.message.reWrite"));
+const submitForm = async () => {
+  await findId(findIdForm);
+  if (foundId.value) {
+    emit("findId");
+  }
+  if (error.value) {
+    await openMessageBox(t("form.message.noMember"));
+    for (const key in findIdForm) {
+      findIdForm[key] = "";
     }
-  });
+  }
 };
 </script>
 
@@ -51,7 +34,6 @@ const submitForm = async (formEl: FormInstance | undefined) => {
   <el-form
     ref="ruleFormRef"
     :model="findIdForm"
-    :rules="rules"
     label-position="top"
     status-icon
   >
@@ -67,7 +49,7 @@ const submitForm = async (formEl: FormInstance | undefined) => {
         color="#004A55"
         size="large"
         style="width: 100%"
-        @click="submitForm(ruleFormRef)"
+        @click="submitForm"
         >{{ $t("member.findId") }}
       </el-button>
     </el-form-item>
--- front/src/components/form/LoginForm.vue
+++ front/src/components/form/LoginForm.vue
@@ -1,50 +1,35 @@
 <script lang="ts" setup>
-import { reactive, ref } from "vue";
-import { useFormValidate } from "@/composables/formValidate";
+import { reactive } from "vue";
 import { useAuth } from "@/composables/auth";
 import { useMessageBox } from "@/composables/messageBox";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 import { useMember } from "@/composables/member";
-import type { FormRules, FormInstance } from "element-plus/es";
 import type { LoginType } from "@/modules/types/form/FormType";
 
 const router = useRouter();
-const { t } = useI18n();
-const { error, isPending, login, isLoggedIn } = useAuth();
 const { getSimpleProfile } = useMember();
+const { error, isPending, login, isLoggedIn } = useAuth();
 const { openMessageBox } = useMessageBox();
-const { validateRequire } = useFormValidate();
-
-const ruleFormRef = ref<FormInstance>();
-const rules = reactive<FormRules>({
-  loginId: [validateRequire("member.loginId")],
-  password: [validateRequire("member.password")],
-});
+const { t } = useI18n();
 
 const loginForm = reactive<LoginType>({
   loginId: "",
   password: "",
 });
 
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
-  await formEl.validate(async (valid) => {
-    if (valid) {
-      await login(loginForm);
-      if (error.value) {
-        await openMessageBox(t("form.message.wrongIdPw"));
-        for (const key in loginForm) {
-          loginForm[key] = "";
-        }
-      } else if (isLoggedIn.value) {
-        await getSimpleProfile();
-        await router.push({ name: "home" });
-      }
-    } else {
-      await openMessageBox(t("form.message.reWrite"));
+const submitForm = async () => {
+  await login(loginForm);
+  if (isLoggedIn.value) {
+    await getSimpleProfile();
+    await router.push({ name: "home" });
+  }
+  if (error.value) {
+    await openMessageBox(t("form.message.wrongIdPw"));
+    for (const key in loginForm) {
+      loginForm[key] = "";
     }
-  });
+  }
 };
 </script>
 
@@ -52,7 +37,6 @@ const submitForm = async (formEl: FormInstance | undefined) => {
   <el-form
     ref="ruleFormRef"
     :model="loginForm"
-    :rules="rules"
     status-icon
     label-position="top"
   >
@@ -68,7 +52,7 @@ const submitForm = async (formEl: FormInstance | undefined) => {
         color="#004A55"
         size="large"
         style="width: 100%"
-        @click="submitForm(ruleFormRef)"
+        @click="submitForm"
         >{{ $t("member.login") }}
       </el-button>
     </el-form-item>
