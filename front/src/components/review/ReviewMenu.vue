32c852e18ff9149107a2d4d4749018a117289456
Leeky0615 <rbdus7174@naver.com>
2022-09-26 17:34:31 +0900

    feat: init review view

---
+++ front/src/components/review/ReviewMenu.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import SmallTitleSlot from "@/components/common/SmallTitleSlot.vue";
+import { useCity } from "@/composables/city/city";
+
+const { cityNames } = useCity();
+</script>
+
+<template>
+  <el-menu
+    active-text-color="#016D7D"
+    background-color="#ffffff"
+    class="review-menu"
+    text-color="#111111"
+  >
+    <SmallTitleSlot :title-line="false" class="ps-2">
+      <el-icon class="pb-1 me-1">
+        <Place />
+        <!--            <component :is="cat.category.icon" />-->
+      </el-icon>
+      <!--      {{ $t(`myPage.${cat.category.code}.title`) }}-->
+      도시
+    </SmallTitleSlot>
+    <el-menu-item v-for="city in cityNames" :key="city.code" class="menu-item">
+      <span class="ms-2">{{ city.value }}</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style lang="scss" scoped>
+.review-menu {
+  border: none;
+  min-height: 600px;
+  background-color: inherit;
+
+  .menu-item {
+    font-size: 1rem;
+    background-color: inherit;
+
+    &:hover {
+      font-size: 1.1rem;
+      background-color: rgba(108, 153, 163, 0.86);
+    }
+
+    &.is-active {
+      font-size: 1.1rem;
+      font-weight: bold;
+    }
+
+    span {
+      padding-left: 20px;
+    }
+  }
+}
+</style>
--- front/src/views/review/ReviewView.vue
+++ front/src/views/review/ReviewView.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import SmallTitleSlot from "@/components/common/SmallTitleSlot.vue";
 import { useMessageBox } from "@/composables/common/messageBox";
+import ReviewMenu from "@/components/review/ReviewMenu.vue";
 
 const { titleMsg } = useMessageBox();
 </script>
@@ -8,20 +8,7 @@ const { titleMsg } = useMessageBox();
 <template>
   <el-row class="container">
     <el-col :span="4">
-      <el-menu
-        active-text-color="#016D7D"
-        background-color="#ffffff"
-        class="review-menu"
-        text-color="#111111"
-      >
-        <SmallTitleSlot :title-line="false" class="ps-2">
-          <el-icon class="pb-1 me-1">
-            <Place />
-            <!--            <component :is="cat.category.icon" />-->
-          </el-icon>
-          {{ $t(`myPage.${cat.category.code}.title`) }}
-        </SmallTitleSlot>
-      </el-menu>
+      <ReviewMenu />
     </el-col>
     <el-col :span="18" class="main-content">
       <el-row>
@@ -41,7 +28,7 @@ const { titleMsg } = useMessageBox();
 <style lang="scss" scoped>
 .container {
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
 
   .main-content {
     display: flex;
