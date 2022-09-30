f5ad5d20e05d753f8af311c2a1d1f611b7c965d2
Leeky0615 <rbdus7174@naver.com>
2022-09-30 21:59:50 +0900

    fix: change naming rule

--- src/main/java/teamproject/lam_server/global/entity/TimePeriod.java
+++ src/main/java/teamproject/lam_server/global/entity/TimePeriod.java
@@ -1,12 +1,11 @@
 package teamproject.lam_server.global.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
@@ -14,8 +13,10 @@ import java.time.LocalDateTime;
 
 @Embeddable
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TimePeriod {
 
     @NotNull
@@ -26,11 +27,5 @@ public class TimePeriod {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime endDateTime;
-
-    public TimePeriod(LocalDateTime start, LocalDateTime end) {
-        this.startDateTime = start;
-        this.endDateTime = end;
-    }
-
 
 }
