f5556e76b634b9bccc9331828577def5b7d88f81
Leeky0615 <rbdus7174@naver.com>
2023-01-14 20:36:08 +0900

    fix: remove unused column

--- src/test/java/com/lam/liveamonthapp/bulk/member/MemberJdbcRepository.java
+++ src/test/java/com/lam/liveamonthapp/bulk/member/MemberJdbcRepository.java
@@ -20,8 +20,8 @@ public class MemberJdbcRepository {
 
     public void batchInsert(List<MemberCreate> subItems) {
         this.jdbcTemplate.batchUpdate(
-                "insert into member (created_date, last_modified_date, birth, email, gender, image, login_id, name, nickname, password, role, social_type, status) values " +
-                        "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                "insert into member (created_date, last_modified_date, birth, email, gender, image, login_id, name, nickname, password, role, status) values " +
+                        "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 new BatchPreparedStatementSetter() {
                     public void setValues(PreparedStatement ps, int i) throws SQLException {
                         MemberCreate member = subItems.get(i);
